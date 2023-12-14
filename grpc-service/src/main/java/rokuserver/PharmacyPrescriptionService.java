ecb06f5504425640858263cb74cde8d80ce1dded
conallmcgrath <conallm742@gmail.com>
2023-12-14 11:34:00 +0000

    this is a slight edit from the other upload, just has the commented code taken out

--- grpc-service/src/main/java/rokuserver/PharmacyPrescriptionService.java
+++ grpc-service/src/main/java/rokuserver/PharmacyPrescriptionService.java
@@ -31,10 +31,11 @@ public class PharmacyPrescriptionService extends PrescriptionServiceGrpc.Prescri
                 try {
                     PrescriptionDTO processedPres = presTransferService.getProcessedPrescription();
                     System.out.println("Processed prescription is:" + processedPres.toString());
-
+                    int pharmacyId = request.getPharmacyId();
                     // build the response
 
                     PharmacyPrescriptionResponse.Builder responseBuilder = PharmacyPrescriptionResponse.newBuilder()
+
                             .setPatientFirstName(processedPres.getPatientFirstName())
                             .setPatientLastName(processedPres.getPatientLastName())
                             .setPatientAddress(processedPres.getPatientAddress())
@@ -51,64 +52,6 @@ public class PharmacyPrescriptionService extends PrescriptionServiceGrpc.Prescri
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
-
-
-
-
-            @Override
-                 */
-
-                int pharmacyId = request.getPharmacyId();
-
-
-
-                /*
-                 // Use the populated PrescriptionDTO from the controller
-        PrescriptionDTO prescriptionDTO = getPrescriptionDTO(request.getPharmacyId());
-
-
-              // Create a PharmacyPrescriptionResponse message and populate it with data from the DTO
-        PharmacyPrescriptionResponse.Builder responseBuilder = PharmacyPrescriptionResponse.newBuilder()
-            .setPatientFirstName(prescriptionDTO.getPatientFirstName())
-            .setPatientLastName(prescriptionDTO.getPatientLastName())
-            .setPatientAddress(prescriptionDTO.getPatientAddress())
-            .setPrescriberName(prescriptionDTO.getPrescriberName())
-            .setPrescriptionId(prescriptionDTO.getPrescriptionId())
-            .setMedicationName(prescriptionDTO.getMedicationName())
-            .setPrescriptionDate(Timestamp.newBuilder().setSeconds(prescriptionDTO.getPrescriptionDate().getTime() / 1000).build())
-            .setDosage(prescriptionDTO.getDosage())
-            .setTargetType(Target.PHARMACY)
-            .setPharmacyId(prescriptionDTO.getPharmacyId());
-
-        // Send the response to the client
-        responseObserver.onNext(responseBuilder.build());;
-
-
-
-                 */
-
-                // prescriptionDTO.setPatientFirstName(request.getPatientFirstName());
-
-
-/*
-                String patientFirstName = request.getPatientFirstName();
-                String patientLastName = request.getPatientLastName();
-                String patientAddress = request.getPatientAddress();
-                String prescriberName = request.getPrescriberName();
-                String medicationName = request.getMedicationName();
-
- */
-/*
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setPrescriptionId(request.getPrescriptionId()).build());
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setPatientFirstName(request.getPatientFirstName()).build());;
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setPatientLastName(request.getPatientLastName()).build());
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setPatientAddress(request.getPatientAddress()).build());
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setPrescriberName(request.getPrescriberName()).build());
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setMedicationName(request.getMedicationName()).build());
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setDosage(request.getDosage()).build());
-                responseObserver.onNext(PharmacyPrescriptionResponse.newBuilder().setPharmacyId(request.getPharmacyId()).build());
-
- */
             }
 
             @Override
